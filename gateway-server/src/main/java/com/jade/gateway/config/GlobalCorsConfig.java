35641d45d4acd4bcfbcfc71553b4e3ffd9d84500
songyx <2250794569@qq.com>
2022-10-11 23:58:57 +0800

    装箱性能测试

--- gateway-server/src/main/java/com/jade/gateway/config/GlobalCorsConfig.java
+++ gateway-server/src/main/java/com/jade/gateway/config/GlobalCorsConfig.java
@@ -1,11 +1,14 @@
 package com.jade.gateway.config;
 
+import cn.hutool.core.date.StopWatch;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class GlobalCorsConfig {
@@ -21,6 +24,18 @@ public class GlobalCorsConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
+    }
+
+    public static void main(String[] args) {
+        StopWatch sw = new StopWatch();
+        sw.start();
+        long sum = 0L;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        sw.stop();
+        long total = sw.getTotal(TimeUnit.MILLISECONDS);
+        System.out.println(total);
     }
 
 }
